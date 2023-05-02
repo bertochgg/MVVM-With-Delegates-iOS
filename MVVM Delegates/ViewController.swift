b72bc1c87c53e8bd6cde7493a2aafd07d35852d4
Humberto <hgarcia@griddynamics.com>
2023-05-02 16:24:54 -0600

    Initial Commit

---
+++ MVVM Delegates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Delegates
+//
+//  Created by Humberto Garcia on 02/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
